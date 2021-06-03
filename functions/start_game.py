a63558a6b87ca4ac0ff100f05104c96c5f197ecf
WhyLev <Levin.korff@gmx.de>
2021-06-03 15:59:54 +0200

    mouselocation Function, main now cleaner

---
+++ functions/start_game.py
@@ -0,0 +1,8 @@
+import pyautogui
+import keyboard
+import time
+import colorama
+import os
+
+def startgame():
+    print("test")
--- main.py
+++ main.py
@@ -9,37 +9,17 @@ from functions.elexier_check import *
 from functions.help import help_commands
 from commands_list import *
 from functions.elexier_check_new import *
+from functions.start_game import *
+from functions.mouselocation import *
 
 colorama.init(autoreset=True)
-decks = [["rage", "valkyrie", "wizard", "rage_barbarian", "baby_dragon", "chr_balloon", "skeleton_horde", "minion_horde"]]
 
 while True:
     command = input(">>")
     if command not in commands_list:
-        print("That is not a command")
+        print("Das ist kein Command")
     if command in commands_list:
-        if command == "mouselocation":
-            while not keyboard.is_pressed("z"):
-                if keyboard.is_pressed("q"):
-                    print(pyautogui.position())
-                    time.sleep(0.5)
-        elif command == "startscan":
-            time.sleep(4)
-            while True:
-                starttime = time.time()
-                location = pyautogui.locateOnScreen(f'./pics/baby_dragonq.png', confidence=0.5)
-                if location:
-                    pyautogui.click(location)
-                    pyautogui.click(778, 667)
-                    endtime = time.time()
-                    print(f"time: {endtime - starttime}")
-
-                endtime = time.time()
-                print(f"time: {endtime-starttime}")
-                time.sleep(2)
-
-
-        elif command == "checkend":
+        if command == "checkend":
             check_game_end()
         elif command == "checkdeck":
             check_deck()
@@ -47,4 +27,9 @@ while True:
             help_commands()
         elif command == "elexiercheck":
             elexier_check_new()
+        elif command == "mouselocation":
+            mouselocation()
+        elif command == "start_game":
+            startgame()
+
 
